4bf4b1581c0f50c178fb002a7873185832d8f825
nazarukiv <nazaruk7649@ukr.net>
2026-03-29 17:18:33 +0100

    start

---
+++ src/main/java/com/nazarukiv/tradeinsight/TradeInsightApplication.java
@@ -0,0 +1,13 @@
+package com.nazarukiv.tradeinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeInsightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeInsightApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nazarukiv/tradeinsight/TradeInsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nazarukiv.tradeinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeInsightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
